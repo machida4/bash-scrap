4ce802fdf0f16380114233901e9fb09b39ed41e1
machida <machida@kmc.gr.jp>
2021-04-23 01:30:01 +0900

    WSL, MacOS, Linuxそれぞれで開けるように

---
+++ scrap.sh
@@ -0,0 +1,24 @@
+# ~/.bashrc
+# Replace scrapbox_project_name with your own!
+
+scrapbox_project_name="machida-memo"
+
+# open Scrapbox page
+function scrap {
+  local title="$@"
+  local scrapbox_url="https://scrapbox.io/$scrapbox_project_name/$title"
+
+  if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+    # WSL
+    explorer.exe $scrapbox_url
+  elif [ "$(uname)" == 'Darwin' ]; then
+    # MacOS
+    open $scrapbox_url
+  elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    # Linux
+    xgd-open $scrapbox_url
+  else
+    echo "Your platform is not supported."
+  fi
+}
+
